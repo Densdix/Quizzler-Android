11443bcf11f6b4c50e8fc1da3f154bba21847f41
Densdix <dixfun333333@gmail.com>
2019-07-04 00:57:13 +0300

    Initial commit

---
+++ app/src/main/java/com/leknos/quizzler/MainActivity.java
@@ -0,0 +1,60 @@
+package com.leknos.quizzler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    // TODO: Declare constants here
+    Button mTrueButton;
+    Button mFalseButton;
+
+    // TODO: Declare member variables here:
+
+
+    // TODO: Uncomment to create question bank
+    //Are you true coder?
+//    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+//            new TrueFalse(R.string.question_1, true),
+//            new TrueFalse(R.string.question_2, true),
+//            new TrueFalse(R.string.question_3, true),
+//            new TrueFalse(R.string.question_4, true),
+//            new TrueFalse(R.string.question_5, true),
+//            new TrueFalse(R.string.question_6, false),
+//            new TrueFalse(R.string.question_7, true),
+//            new TrueFalse(R.string.question_8, false),
+//            new TrueFalse(R.string.question_9, true),
+//            new TrueFalse(R.string.question_10, true),
+//            new TrueFalse(R.string.question_11, false),
+//            new TrueFalse(R.string.question_12, false),
+//            new TrueFalse(R.string.question_13,true)
+//    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button True Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button False Pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
